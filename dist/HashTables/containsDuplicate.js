38459db51d87697dd9b079a4696e60d0bdc7df26
osas2211 <osaretin.frank10@gmail.com>
2022-05-28 08:01:03 +0100

    containsDuplicate_Solved

---
+++ dist/HashTables/containsDuplicate.js
@@ -0,0 +1,21 @@
+"use strict";
+const containsDuplicate = (nums) => {
+    const cache = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (cache[nums[i]] !== undefined) {
+            return true;
+        }
+        else {
+            cache[nums[i]] = i;
+        }
+    }
+    return false;
+};
+const test = containsDuplicate([1, 2, 3, 4, 5, 1]);
+const test2 = containsDuplicate([1, 2, 3, 1]);
+const test3 = containsDuplicate([1]);
+const test4 = containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2]);
+console.log(test);
+console.log(test2);
+console.log(test3);
+console.log(test4);
