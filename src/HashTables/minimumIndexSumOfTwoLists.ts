a34bb9af830919db88e3578ca6210f6534892812
osas2211 <osaretin.frank10@gmail.com>
2022-06-04 23:07:43 +0100

    minimumIndexSumOfTwoLists__solved

---
+++ src/HashTables/minimumIndexSumOfTwoLists.ts
@@ -0,0 +1,32 @@
+const minimumIndexSumOfTwoLists = (list1: string[], list2: string[])=>{
+    let cache:any= {}
+    let common:any = {}
+    const minIndex: string[] = []
+    let min = +Infinity
+    for(let i = 0; i<list1.length; i++){
+        if(cache[list1[i]] !== undefined){
+            cache[list1[i]] += i
+        }
+        else{
+            cache[list1[i]] = i
+        }
+    }
+
+    for(let i = 0; i<list2.length; i++){
+        if(cache[list2[i]] !== undefined){
+            common[list2[i]] = cache[list2[i]] + i
+            min = Math.min(min , common[list2[i]])
+        }
+    }
+
+    Object.keys(common).forEach(key => {
+        if(common[key] === min){
+            minIndex.push(key)
+        }
+    })
+
+    return minIndex
+}
+
+const tester = minimumIndexSumOfTwoLists(["biggs", "kada", "mat ice", "filmhouse"], ["kada", "biggs", "filmhouse", "chicken republic"])
+console.log(tester);
